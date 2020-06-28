e2a6a37f8b4246eecbdf9b23227886ca49c1edf1
afetzer <aayla.fetzer@gmail.com>
2020-06-28 00:29:04 -0600

    Added .pdf and .csv search

--- README.md
+++ README.md
@@ -1,8 +1,24 @@
 # BigSearch
 BigSearch is a simple Python program that recursively indexes the directory tree it's placed in and records the indexes to a SQLite database so massive files can be searched more easily. This was originally designed to help journalists search the BlueLeaks dump for keywords.
 ## Installation
-Install Python from [python.org](https://www.python.org/downloads/) if you're using Windows or this guide from [python-guide.org](https://docs.python-guide.org/starting/install3/osx/) if you're using a Mac. Next:
+Install Python from [python.org](https://www.python.org/downloads/) if you're using 
+Windows or from the App Store if you're using a Mac.
 
 1. Download and extract BigSearch to the directory you want to index and search
 2. Navigate a terminal/command prompt to the directory and run ``pip install -r requirements.txt``
 3. Run app.py (``python3 app.py`` if you're running it from the terminal) and the script will begin to index the directory. This will only happen once unless you delete ``BigSearchDB.sqlite``
+
+## Advanced Usage
+To get started using BigSearch, simply run ``python3 app.py`` after installing the 
+requirements and the rest will be handled for you. However, there are more advanced use
+options of BigSearch if you want to get more data or dig deeper. To access them, you can
+use these command line arguments
+
+- ``-r / --regex`` Will allow you to search for any regular expression, as opposed to the 
+default ``%query%``
+- ``-c / --csv`` Will save the contents of all .csv files into the database to enable searching
+them quickly
+- ``-p / --pdf`` Will use pdftotext to attempt to read the text content of all pdf files and
+save it to the database to enable searching them quickly. Be aware, because of the [limitations](https://stackoverflow.com/a/17099530/13026048)
+of converting PDF files to text files, this is not guaranteed to be an accurate search, but has the potential to be useful when
+searching dumps of PDF files generated in software like Microsoft Word (as opposed to read by a scanner, etc.)
--- app.py
+++ app.py
@@ -1,9 +1,28 @@
 import os
-import json
+import argparse
+
+# File processing imports
+import csv
+import PyPDF2
+import re
+
+# SQL Alchemy imports
 from sqlalchemy import create_engine
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+
+# Command line argument handling
+parser = argparse.ArgumentParser(description="Search a large directory tree")
+parser.add_argument("-r", "--regex", help="Allow any regular expression to be entered", action="store_true")
+parser.add_argument("-v", "--verbose", help="Print file names as they're indexed as opposed to a counter",
+                    action="store_true")
+group = parser.add_argument_group("File Options")
+group.add_argument("-c", "--csv", help="Store/search the contents of csv files in database", action="store_true")
+group.add_argument("-p", "--pdf", help="Store/search the contents of pdf files in database", action="store_true")
+parser.add_argument("query", help="Query to search for")
+
+args = parser.parse_args()
 
 Base = declarative_base()
 
@@ -16,35 +35,134 @@ engine = create_engine("sqlite:///BigSearchDB.sqlite")
 
 
 class FilePath(Base):
-    __tablename__ = "files"
+    """
+    File path for indexing files in database
+    """
+    __tablename__ = "FilePaths"
 
     path = Column(String, primary_key=True)
+    name = Column(String)
 
     def __repr__(self):
-        return f"<FilePath(path={self.path})>"
+        return f"<FilePath(path={self.name})>"
+
+    def __init__(self, path, name):
+        self.path = path
+        self.name = name
+
+
+class CsvFile(Base):
+    """
+    CSV Files and their contents for searching
+    """
+    __tablename__ = "CsvFiles"
+
+    path = Column(String, primary_key=True)
+    content = Column(String)
+
+    def __repr__(self):
+        return f"<CsvFile(path={self.path}>"
+
+    def __init__(self, path):
+        self.path = path
+        with open(self.path, "r") as openFile:
+            self.content = openFile.read()
+
+
+class PdfFile(Base):
+    """
+    PDF File Paths and their contents for searching
+    """
+    __tablename__ = "PdfFiles"
+
+    path = Column(String, primary_key=True)
+    content = Column(String)
+
+    def __repr__(self):
+        return f"<PdfFile(path={self.path}>"
+
+    def __init__(self, path):
+        self.path = path
+        content = ""
+        pdfFile = PyPDF2.PdfFileReader(path)
+        pdfPages = pdfFile.numPages
+        for n in range(0, pdfPages):
+            page = pdfFile.getPage(i)
+            pageText = page.extractText()
+            content += pageText
+        self.content = content
 
 
 Base.metadata.create_all(engine)
 Session = sessionmaker(bind=engine)
 session = Session()
 
-# Generate the file list
+# Generate the database
 if generateFilesList:
     print("Indexing Files")
     i = 0
     for root, dirs, files in os.walk("."):
         for name in files:
             i += 1
-            print(f"Indexed {i} files", end="\r")
+            if args.verbose:
+                print(f"Indexed {i} files: {name}")
+            else:
+                print(f"Indexed {i} files", end="\r")
             newPath = os.path.join(root, name)
+            newPathName, newPathExtension = os.path.splitext(newPath)
+            # Add file path to database
             session.add(
-                FilePath(path=newPath)
+                FilePath(newPath, newPathName)
             )
+            # Add csv file to database if enabled
+            if args.csv and newPathExtension == ".csv":
+                session.add(
+                    CsvFile(newPath)
+                )
+            # Add pdf file content to database if enabled
+            if args.pdf and newPathExtension == ".pdf":
+                session.add(
+                    PdfFile(newPath)
+                )
     print("\nWriting index to database file. This might take a while.")
     session.commit()
     print(f"Indexing Complete")
 
-query = input("Search Query: ")
+# Search the database
+if args.regex:
+    query = args.query
+else:
+    query = f"%{args.query}%"  # Basic regex search if the user doesn't set the --regex flag
+
+print(f"Search Query: {args.query}")
+
+print("-" * 15 + "Filenames" + "-" * 15)
+fileMatches = False
 for file in session.query(FilePath). \
-        filter(FilePath.path.ilike(f"%{query}%")):
+        filter(FilePath.name.ilike(query)):
+    fileMatches = True
     print(file.path)
+if not fileMatches:
+    print("Query was not found in any filenames")
+
+# Search through pdf files if requested
+if args.pdf:
+    print("-" * 15 + "PDF Files" + "-" * 15)
+    fileMatches = False
+    for file in session.query(PdfFile). \
+            filter(PdfFile.content.ilike(query)):
+        print(file.path)
+        fileMatches = True
+    if not fileMatches:
+        print("Query was not found in any PDF files")
+
+# Search through csv files if requested
+if args.csv:
+    print("-" * 15 + "CSV Files" + "-" * 15)
+    fileMatches = False
+    for file in session.query(CsvFile). \
+            filter(CsvFile.content.ilike(query)):
+        print(file.path)
+        fileMatches = True
+    if not fileMatches:
+        print("Query was not found in any CSV files")
